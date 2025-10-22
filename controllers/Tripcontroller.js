76078b4963ea4751f9a36f90feeb8529bbae8e34
hpgkareem <kareemfunyboy@gmail.com>
2025-10-22 10:46:24 +0300

    LAB 2

--- controllers/Tripcontroller.js
+++ controllers/Tripcontroller.js
@@ -10,3 +10,51 @@ const retrieveAllTrips = (req, res) => {
         data: allTrips,
     });
 };
+
+const createtrip =(req,res) => {
+    const {
+        destinationName,
+        location,
+        continent,
+        language,
+        description,
+        flightCost,
+        accommodationCost,
+        mealCost,
+        visaCost,
+        transportationCost,
+        currencyCode,}=req.body;
+
+
+const newTrip=
+{
+id:trips.length+1,
+destinationName,
+        location,
+        continent,
+        language,
+        description,
+        flightCost,
+        accommodationCost,
+        mealCost,
+        visaCost,
+        transportationCost,
+        currencyCode,
+
+}
+trips.push(newTrip);
+
+}
+
+const deleteTripById=(req,res)=>{
+    const id=Number(req.params.id);
+    const index=trips.findIndex(t=>t.id==id);
+    trips.slice(index,1);
+    res.status(200).json({
+        status:'success',
+        message:'Trip deleted successfully'
+    });
+}
+
+module.exports
+{retrieveAllTrips,createTrip,deleteTripById};
