237372dfd2d35b22396875b676b9567dd5085e06
hpgkareem <kareemfunyboy@gmail.com>
2025-12-06 13:00:37 +0200

    milestone 4

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const tripRouter = require('./routes/TripRoutes.js');
+const userRouter = require('./routes/userRoutes.js');
+const authRouter = require('./routes/authRoutes.js');
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Create an instance of the Express application
 const app = express();
-const TripRouter=require('./routes/TripRoutes.js'); //
 
-
+// Use middleware to parse JSON data from request bodies
 app.use(express.json());
-app.use('/api/v1/trips', TripRouter);
 
 
-module.exports =
-{
-    app,
+app.use('/api/v1/trips', tripRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/auth', authRouter);
+
+module.exports = {
+  app,
 };
--- server.js
+++ server.js
@@ -1,19 +1,21 @@
-const {app} = require('/index.js');
-
+const { app } = require('./index.js');
 // added db initialization to server
-
 const db_access = require('./db.js');
-const db = db_access. db;
+const db = db_access.db;
 
 const PORT = 3000;
 
 // Initialize database tables
-db. serialize(() => {
-    db.run(db_access.createTripTable, (err) => {
-      if (err) console.log('Error creating trip table:', err.message);
-    });
+db.serialize(() => {
+  db.run(db_access.createTripTable, (err) => {
+    if (err) console.log('Error creating trip table:', err.message);
+  });
+  db.run(db_access.createUserTable, (err) => {
+    if (err) console.log('Error creating user table:', err.message);
+  });
+});
 
-    db.run(db_access.createUserTable, (err) => {
-      if (err) console.log('Error creating trip table:', err.message);
-    });
+// Start listening on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
